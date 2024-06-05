502062bd130f6bdff2446b0972b0d10b7c9be31c
Maryna <127458923+marnka@users.noreply.github.com>
2024-06-05 13:43:52 +0300

    Update test_palindrome.py

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,5 +1,8 @@
 import unittest
+import sys
+import io
 from palindrome import is_palindrome
+from palindrome import main
 
 class TestPalindromeChecker(unittest.TestCase):
 
@@ -37,6 +40,47 @@ class TestPalindromeChecker(unittest.TestCase):
             is_palindrome(123)
         with self.assertRaises(TypeError):
             is_palindrome(None)
+
+
+    def setUp(self):
+        self.stdin_backup = sys.stdin
+        self.stdout_backup = sys.stdout
+        self.stderr_backup = sys.stderr
+    
+    def tearDown(self):
+        sys.stdin = self.stdin_backup
+        sys.stdout = self.stdout_backup
+        sys.stderr = self.stderr_backup
+    
+    def test_main_without_errors(self):
+        input_data = "radar\n"
+        expected_output = "True\n"
+
+        sys.stdin = io.StringIO(input_data)
+        sys.stdout = io.StringIO()
+
+        with self.assertRaises(SystemExit) as cm:
+            main()
+
+        self.assertEqual(cm.exception.code, 0)
+        self.assertEqual(sys.stdout.getvalue(), expected_output)
+    
+    def test_main_with_errors(self):
+        input_data = " "
+        expected_error_message = "Error: Input cannot be empty\n"
+
+        sys.stdin = io.StringIO()  # Змініть на цей рядок
+        sys.stdin.write(input_data)
+        sys.stdin.seek(0)
+        sys.stdout = io.StringIO()
+        sys.stderr = io.StringIO()
+
+        with self.assertRaises(SystemExit) as cm:
+            main()
+
+        self.assertEqual(cm.exception.code, 1)
+        self.assertEqual(sys.stderr.getvalue(), expected_error_message)
+
 
 if __name__ == "__main__":
     unittest.main()
